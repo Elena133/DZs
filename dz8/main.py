7f9b1dd9c28b6c3e15f7f69af2c5bffe4d46b1c6
Elena133 <37879530+Elena133@users.noreply.github.com>
2019-10-30 15:31:49 +0200

    Create main.py
    main file of dz8 added

---
+++ dz8/main.py
@@ -0,0 +1,44 @@
+import figures
+
+figuresList = []
+
+for i in range(3):
+    choice = input("Круг(к), прямоугольник(п) или треугольник(т): ")
+
+    if choice == 'к':
+        figure = figures.Circle(float(input("Радиус: ")))
+
+    elif choice == 'п':
+        l = float(input("Длина: "))
+        w = float(input("Ширина: "))
+
+        figure = figures.Rectangle(w, l)
+
+    elif choice == 'т':
+        a = float(input("Первая сторона: "))
+        b = float(input("Вторая сторона: "))
+        c = float(input("Третья сторона: "))
+
+        figure = figures.Triangle(a, b, c)
+
+    figuresList.append(figure)
+
+choice = input("Максимальная площадь (пл)б максимальный периметр(пр): ")
+if choice == 'пл':
+    maxS = 0
+    n = ''
+    for i in figuresList:
+        s = i.getSquare()
+        if maxS < s:
+            maxS = s
+            n = i.name
+elif choice == 'пр':
+    maxS = 0
+    n = ''
+    for i in figuresList:
+        s = i.getPer()
+        if maxS < s:
+            maxS = s
+            n = i.name
+
+print(maxS,n)
