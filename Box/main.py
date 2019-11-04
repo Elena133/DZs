283d328895806f4bbb019d4be5121c5909a3cdba
Elena133 <37879530+Elena133@users.noreply.github.com>
2019-11-04 13:26:01 +0200

    Update main.py
    main code added:
    - calculation of winer
    -output

--- Box/main.py
+++ Box/main.py
@@ -3,3 +3,24 @@ import time
 
 fighterOne = boxMy(input("First Fighter Name:  "))
 fighterTwo = boxMy(input("Second Fighter Name:  "))
+
+healthOne = fighterOne.health
+healthTwo = fighterTwo.health
+
+while healthOne > 0 or healthTwo > 0:
+    dOne = fighterOne.getDamage()
+    dTwo = fighterTwo.getDamage()
+    healthOne = healthOne - dTwo
+    healthTwo = healthTwo - dOne
+    if healthTwo > 0:
+        print(healthTwo, fighterTwo.name,"'s remained healthy, after ", fighterOne.name, 'hit for ', dOne)
+        time.sleep(2)  # Delays for 2 seconds.
+    else:
+        print(fighterTwo.name, 'lost')
+        break
+    if healthOne > 0:
+        print(healthOne, fighterOne.name,"'s remained healthy, after ", fighterTwo.name, 'hit for ', dTwo)
+        time.sleep(2)  # Delays for 2 seconds.
+    else:
+        print(fighterOne.name, 'lost')
+        break
