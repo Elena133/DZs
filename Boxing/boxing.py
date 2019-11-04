87f3c1067bd06f5169c2170a504197c3049ea09b
Elena133 <37879530+Elena133@users.noreply.github.com>
2019-11-04 16:39:35 +0200

    Update boxing.py
    comments added

--- Boxing/boxing.py
+++ Boxing/boxing.py
@@ -6,11 +6,11 @@ class Boxer:
         self.name = name
         self.health = 100
 
-    def receiveDamage(self, hit):
+    def receiveDamage(self, hit):      #count damage 
         self.health = self.health - hit
         print('{} has {} hp left'.format(self.name, self.health))
 
-    def hit(self, boxer):
+    def hit(self, boxer):               #random damage applied to boxer
         hit = random.randrange(5, 33)
         boxer.receiveDamage(hit)
         print('{} hit {} for {}'.format(self.name, boxer.name, hit))
